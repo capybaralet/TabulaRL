dff94227533564f48a31ad6b8c68b0ecac5bca2b
John Salvatier <jsalvatier@gmail.com>
2016-07-26 21:04:11 +0100

    minor

--- src/dk_experiment.py
+++ src/dk_experiment.py
@@ -5,24 +5,26 @@ import query_functions
 import finite_tabular_agents
 
 seed = np.random.randint(10000) #1
-numpy_rng  = np.random.RandomState(seed)
+numpy_rng = np.random.RandomState(seed)
 
 from feature_extractor import FeatureTrueState
 from experiment import run_finite_tabular_experiment
 
-# PSRL / ENV params
+# AGENT
+
 alg = finite_tabular_agents.PSRL
 nEps=100
+query_cost = 2
 epLen = 50
 scaling = 1
-query_function = query_functions.QueryFirstNVisits(env, agent, query_cost, 4)
+
+
+# HYPER
+
+grid_width = 10
+prob_zero_reward = .9
 
 targetPath = 'test.csv'
-
-# hyper-parameters:
-grid_width = 10
-query_cost = .01
-prob_zero_reward = .9
 
 # states (lexical order)
 states = range(grid_width**2)
@@ -30,6 +32,7 @@ states = range(grid_width**2)
 # reward probabilities
 reward_probabilities = numpy_rng.binomial(1, 1 - prob_zero_reward, len(states)) * numpy_rng.uniform(0, 1, len(states))
 print reward_probabilities
+
 
 # Make the environment
 env = gridworld.make_gridworld(grid_width, epLen, reward_probabilities)
@@ -40,6 +43,8 @@ f_ext = FeatureTrueState(env.epLen, env.nState, env.nAction, env.nState)
 agent = alg(env.nState, env.nAction, env.epLen,
                           scaling=scaling, 
                           P_true=env.P, R_true=False)
+
+query_function = query_functions.QueryFirstNVisits(env, agent, query_cost, 4)
 
 # Run the experiment
 run_finite_tabular_experiment(agent, env, f_ext, nEps, seed,
