5d97df486296f7bbb37268aed8f9627d39a3ddb7
John Salvatier <jsalvatier@gmail.com>
2016-08-24 19:15:54 +0100

    add simple experiment

--- TabulaRL/estimate_optimal_queries.py
+++ TabulaRL/estimate_optimal_queries.py
@@ -81,23 +81,7 @@ def sample_real_mdp(agent):
     return gridworld.make_mdp(agent.nState, agent.nAction, agent.epLen, R, P, 0)
 
 import plot
-from gridworld_plot import plot 
-action_directions = [
-        plot.CENTER, 
-        plot.UP, 
-        plot.RIGHT, 
-        plot.DOWN, 
-        plot.LEFT]
 
-def plotQ(env, mdp, agent, timestep=0):
-    fig, ax = plot.grid_plot(env)
-    tq = agent.qVals
-    q = { s : tq[s,0] for s in range(env.nState) }
-
-    plot.plotQ(ax, env, q, plot.plot_labeled_arrows(action_directions))
-    plot.plotR(ax, env, mdp.R)
-    plot.plotRBelief(ax, env, agent.R_prior)
-    fig.show()
 
 
 def rollout_performance(agent, mdp): 
---
+++ TabulaRL/nexperiments.py
@@ -0,0 +1,61 @@
+import numpy as np
+import argparse
+import gridworld
+import finite_tabular_agents
+from query_functions import QueryFixedFunction
+
+from feature_extractor import FeatureTrueState
+from experiment import run_finite_tabular_experiment
+import pandas 
+
+from sqr import *
+
+seed = 2 
+numpy_rng = np.random.RandomState(seed)
+
+#ENV
+grid_width=4
+epLen = 2 * grid_width - 1 + 8
+num_episodes = 53
+reward_sd = 2
+
+env = gridworld.make_gridworld(grid_width, epLen, {}, reward_sd)
+env.num_episodes = num_episodes
+
+
+def randomWorld():
+    rewards = np.zeros(env.nState)
+    rewards[np.random.randint(env.nState)] = 1
+    rewards = gridworld.reward_for_action(rewards, action=0)
+
+    world = gridworld.make_gridworld(grid_width, epLen, rewards, reward_sd)
+
+    world.num_episodes = num_episodes
+    return world
+
+# AGENT
+query_cost=1.5
+reward_tau = reward_sd**-2
+
+ns = [0,1,2,4,8]
+qfunctions = [ QueryFixedFunction(query_cost, lambda s,a: (a==0)*n) for n in ns ]
+
+
+
+prior = genPrior(env, { (s,0) : (0, 1) for s in range(env.nState) }, (0, 10e10))
+
+agents = [ 
+        MakeSQRAgent(reward_tau, env, prior, qfunctions, iters=20),
+        MakePSRLLimitedQueryAgent(reward_tau, env, prior, QueryFixedFunction(query_cost, lambda s,a: (a==0)*1)),
+        MakePSRLLimitedQueryAgent(reward_tau, env, prior, QueryFixedFunction(query_cost, lambda s,a: (a==0)*0))
+        ]
+labels = ['sqr', 'limit query n=1','limit query n=0' ]
+worlds = [ randomWorld() for i in range(50) ]
+
+
+
+data = performance_rollouts(worlds, agents, labels)
+data = pandas.DataFrame(data, columns='n,R,cumReward,cumQueryCost,perf,cumRegret,agent'.split(','))
+data.to_csv('sqr_experiment.csv')
+print "n chosen", agents[0].query_function.func(0,0)
+
