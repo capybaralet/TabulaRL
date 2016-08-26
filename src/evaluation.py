975333444944d68b51451fb63659d88cac2bd8c8
David Krueger <davidscottkrueger@gmail.com>
2016-08-26 17:53:28 +0100

    move old stuff to bottom of eval

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,47 +1,4 @@
 from pylab import *
-load_str = 'junk.txt'
-def plot_regret(load_str=load_str):
-    eps = []
-    cum_regrets = []
-    perfs = []
-    for line in open(load_str).readlines()[1:]:
-        ep, _, _, cum_regret, _, perf  = line.split(',')
-        eps.append(ep)
-        cum_regrets.append(cum_regret)
-        perfs.append(perf)
-    figure()
-    plot(eps, cum_regrets)
-    figure()
-    plot(eps, perfs)
-
-
-def compute_performance(cum_reward, perf, query_cost_multiplier):
-    query_cost = cum_reward - perf
-    return cum_reward - query_cost * query_cost_multiplier
-
-
-def plot_performance(load_str, query_cost=None):
-    filename = load_str.split('/')[-1]
-    orig_query_cost = float(filename.split('__')[0].split("=")[1])
-    if query_cost is None:
-        query_cost = orig_query_cost
-    eps = []
-    cum_rewards = []
-    orig_perfs = []
-    perfs = []
-    orig_query_costs = []
-    for line in open(load_str).readlines()[1:]:
-        ep, _, cum_reward, _, _, perf  = line.split(',')
-        eps.append(int(ep))
-        cum_rewards.append(float(cum_reward))
-        orig_perfs.append(float(perf))
-        orig_query_costs.append(orig_perfs[-1] - cum_rewards[-1])
-        perfs.append(cum_rewards[-1] + orig_query_costs[-1] * query_cost / orig_query_cost)
-    #figure()
-    plot(eps, perfs)
-    return perfs
-    
-
 #----------------------------------------------------
 # Below: for dk_exp_script (Aug 24)
 # TODO: more plotting for SQR/ASQR
@@ -292,4 +249,48 @@ def fixed_n_plots(avg_perf):
 
 
 
+
+#----------------------------OLDER---------------------------
+load_str = 'junk.txt'
+def plot_regret(load_str=load_str):
+    eps = []
+    cum_regrets = []
+    perfs = []
+    for line in open(load_str).readlines()[1:]:
+        ep, _, _, cum_regret, _, perf  = line.split(',')
+        eps.append(ep)
+        cum_regrets.append(cum_regret)
+        perfs.append(perf)
+    figure()
+    plot(eps, cum_regrets)
+    figure()
+    plot(eps, perfs)
+
+
+def compute_performance(cum_reward, perf, query_cost_multiplier):
+    query_cost = cum_reward - perf
+    return cum_reward - query_cost * query_cost_multiplier
+
+
+def plot_performance(load_str, query_cost=None):
+    filename = load_str.split('/')[-1]
+    orig_query_cost = float(filename.split('__')[0].split("=")[1])
+    if query_cost is None:
+        query_cost = orig_query_cost
+    eps = []
+    cum_rewards = []
+    orig_perfs = []
+    perfs = []
+    orig_query_costs = []
+    for line in open(load_str).readlines()[1:]:
+        ep, _, cum_reward, _, _, perf  = line.split(',')
+        eps.append(int(ep))
+        cum_rewards.append(float(cum_reward))
+        orig_perfs.append(float(perf))
+        orig_query_costs.append(orig_perfs[-1] - cum_rewards[-1])
+        perfs.append(cum_rewards[-1] + orig_query_costs[-1] * query_cost / orig_query_cost)
+    #figure()
+    plot(eps, perfs)
+    return perfs
+    
 
