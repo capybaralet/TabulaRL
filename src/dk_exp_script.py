24360aca95be5d1eb94ad6e54b142d4ae4b233a6
David Krueger <davidscottkrueger@gmail.com>
2016-08-30 18:07:36 +0100

    clean-up dk_exp_script; rename algorithm --> query_fn

--- src/dk_exp_script.py
+++ src/dk_exp_script.py
@@ -6,8 +6,6 @@ import TabulaRL.gridworld as gridworld
 import TabulaRL.query_functions as query_functions
 import TabulaRL.finite_tabular_agents as finite_tabular_agents
 from TabulaRL.feature_extractor import FeatureTrueState
-#from experiment import run_finite_tabular_experiment
-#from TabulaRL.dk_run_finite_tabular_experiment import run_finite_tabular_experiment
 from TabulaRL.environment import make_stochasticChain
 #np.random.seed(1)
 
@@ -21,8 +19,8 @@ t1 = time.time()
 #   log visit/query counts, desired query sets
 
 # TODO: don't use env as a variable name!
-
-# TODO: note that increasing the max_reward (with num_states) effects the validity of the prior!!!!
+# TODO: chain max_reward HACK: increasing max_reward effects the validity of the prior!!!!
+#   Use deterministic chain!
 
 #-----------------------------------------------------------------------------------
 # USEFUL FUNCTIONS
@@ -37,59 +35,25 @@ def sample_gaussian(loc, scale, shape):
     else:
         return np.random.normal(loc, scale, shape)
 
-# THINGS TO DEAL WITH JOHN's GRIDWORLD mod:
+def update_gaussian_posterior_mean(prior, observations, tau=1):
+    mu0, tau0 = prior
+    tau1 = tau0 + tau * len(observations)
+    mu1 = (mu0 * tau0 + sum(observations) * tau) / tau1
+    return mu1
 
-# modify an agent's prior so that it knows that only the stay actions provide reward
-def modifyPrior(prior): 
-   def nonStayKnown(sa, p ): 
-       #non 'stay' actions have infinite precision
-       _, action = sa
-       mu, tau = p
-       if action != 0: 
-           return (mu, 1e11)
-       else: 
-           return (mu, tau)
-   return { k : nonStayKnown(k, v) for k,v in prior.iteritems() }
-
-# modify query function to not query specific state action pairs (i.e. any actions other than stay action)
-# e.g. query_function=QueryFixedFunction(query_cost, lambda s,a: (a==0) * n)
-class QueryFixedFunction(query_functions.QueryFunction):
-    def __init__(self, queryCost, func):
-        self.__dict__.update(locals())
-        self.visit_count = defaultdict(lambda :0)
-
-    def __call__(self, state, action, episode, timestep):
-        query = self.will_query(state, action)
-        self.visit_count[state, action] += 1
-        return query, query*self.queryCost
-
-    #We can rewrite all query functions to use this subroutine when called
-    def will_query(self, state, action):
-        return self.visit_count[state, action] < self.func(state, action)
-
-# end USEFUL FUNCTIONS
 #-----------------------------------------------------------------------------------
-
-
 # SETUP
 import argparse
 parser = argparse.ArgumentParser()
-# we only need different costs if we're using ASQR
-#parser.add_argument('--query_cost', type=float, default=1.)
-query_cost = 1.
 parser.add_argument('--log_n_max', type=int, default=10)
-#parser.add_argument('--normalize_rewards', type=int, default=0)
-normalize_rewards=0
 parser.add_argument('--log_num_episodes', type=int, default=15)
 parser.add_argument('--num_R_samples', type=int, default=100)
 parser.add_argument('--environment', type=str, default='chain5')
 parser.add_argument('--agent', type=str, default='PSRLLimitedQuery')
-parser.add_argument('--algorithm', type=str, default='fixed_n')
+parser.add_argument('--query_fn', type=str, default='fixed_n')
 args = parser.parse_args()
 args_dict = vars(args)
 locals().update(args_dict) # add all args to local namespace
-
-assert query_cost > 0
 
 settings_str = '__'.join([arg + "=" + str(args_dict[arg]) for arg in sorted(args_dict.keys())])
 
@@ -149,6 +113,7 @@ initial_env = env
 num_episodes_remaining = 2**log_num_episodes
 n_max = 2**log_n_max
 ns = np.hstack((np.array([0,]), 2**np.arange(log_n_max)))
+query_cost = 1.
 
 # save results here:
 num_queries = np.empty((num_R_samples, log_num_episodes+1, log_n_max+1))
@@ -160,42 +125,34 @@ for kk in range(num_R_samples):
     print "beginning experiment #", kk
     env = copy.deepcopy(initial_env)
 
-    if algorithm in ['SQR', 'ASQR']: # use a sampled environment
+    if query_fn in ['SQR', 'ASQR']: # use a sampled environment
         sampled_R, sampled_P = initial_agent.sample_mdp()
         env.R = {kk:(sampled_R[kk], 1) for kk in sampled_R}
         env.P = sampled_P
-        # TODO: use P_true or sampled_P here? (for now they are the same...)
         returns_max_min[kk,0] = initial_agent.compute_qVals(sampled_R, sampled_P)[1][0][0]
         returns_max_min[kk,1] = - initial_agent.compute_qVals({kk: -sampled_R[kk] for kk in sampled_R}, sampled_P)[1][0][0]
     else:
         mean_rewards = {kk:env.R[kk][0] for kk in env.R}
         returns_max_min[kk,0] = initial_agent.compute_qVals(mean_rewards, env.P)[1][0][0]
-        #returns_max_min[kk,1] = - initial_agent.compute_qVals({kk: -sampled_R[kk] for kk in sampled_R}, sampled_P)[1][0][0]
+        returns_max_min[kk,1] = - initial_agent.compute_qVals({kk: -mean_rewards[kk] for kk in mean_rewards}, sampled_P)[1][0][0]
 
-    # TODO: can we just use the same seed or something?
     sampled_rewards = {(s,a) : sample_gaussian(env.R[s,a][0], env.R[s,a][1], n_max) for (s,a) in env.R.keys()}
-    # is this still needed??
-    first_n_sampled_rewards = [{sa: sampled_rewards[sa][:n] for sa in sampled_rewards} for n in range(n_max + 1)]
     for ind, n in enumerate(ns):
         agent = copy.deepcopy(initial_agent)
-        if environment.startswith('grid'): # FIXME: update agent
-            query_function = QueryFixedFunction(query_cost, lambda s,a: (a==0) * n)
+        if environment.startswith('grid'):
+            query_function = query_functions.QueryFixedFunction(query_cost, lambda s,a: (a==0) * n)
+            agent.R_prior = finite_tabular_agents.modifyPrior(agent.R_prior)
         else:
             query_function = query_functions.QueryFirstNVisits(query_cost, n)
         query_function.setAgent(agent)
 
-        if algorithm=="ASQR": # update posterior, compute expected returns
-            updated_R = {}
-            for [s,a] in first_n_sampled_rewards[n]: # TODO: clean-up (make a separate function?)
-                mu0, tau0 = agent.R_prior[s,a]
-                num_samples = len(first_n_sampled_rewards[n][s,a])
-                tau1 = tau0 + agent.tau * num_samples
-                mu1 = (mu0 * tau0 + sum(first_n_sampled_rewards[n][s,a]) * agent.tau) / tau1
-                updated_R[s,a] = mu1
-            updated_P = sampled_P # TODO: use ASQR to learn about P somehow??
+        if query_fn=="ASQR": # update posterior, compute expected returns
+            # just the mean
+            updated_R = {sa: update_gaussian_posterior_mean(agent.R_prior[sa], sampled_rewards[sa][:n], agent.tau) for sa in sampled_rewards}
+            updated_P = sampled_P # TODO: use ASQR to learn about P!
             expected_returns = agent.compute_qVals_true(updated_R, updated_P, sampled_R, sampled_P)[0]
             returns[kk, :, ind] = expected_returns * 2**np.arange(log_num_episodes+1)
-            num_queries[kk, :, ind] = n * sum([agent.query_function.will_query(s,a) for [s,a] in first_n_sampled_rewards[n]])
+            num_queries[kk, :, ind] = n * sum([agent.query_function.will_query(s,a) for [s,a] in sampled_rewards])
         else: # Run an experiment 
             nEps = num_episodes_remaining
             # --------------- modified from dk_run_finite_tabular_experiment ------------------
@@ -217,8 +174,8 @@ for kk in range(num_R_samples):
                     cumQueryCost += queryCost
 
                     reward, newState, pContinue = env.advance(action)
-                    if query and first_n_sampled_rewards[n] is not None:
-                        reward = first_n_sampled_rewards[n][oldState, action][agent.query_function.visit_count[oldState, action] - 1]
+                    if query:
+                        reward = sampled_rewards[oldState, action][agent.query_function.visit_count[oldState, action] - 1]
                     cumReward += reward 
                     agent.update_obs(oldState, action, reward, newState, pContinue, h, query)
 
@@ -227,10 +184,9 @@ for kk in range(num_R_samples):
                     num_queries[kk, int(np.log2(ep)), ind] = cumQueryCost / query_cost
 
             # ---------------------------------------------------------------------
-    """
-    np.save(save_str + 'num_queries', num_queries)
-    np.save(save_str + 'returns', returns)
-    np.save(save_str + 'returns_max_min', returns_max_min)
-    """
+    if 0:
+        np.save(save_str + 'num_queries', num_queries)
+        np.save(save_str + 'returns', returns)
+        np.save(save_str + 'returns_max_min', returns_max_min)
 
 
